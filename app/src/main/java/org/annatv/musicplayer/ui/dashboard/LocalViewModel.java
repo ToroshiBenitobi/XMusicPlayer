7250b6c8cf77586797dbed3bdcbfefa9816ae6db
Anarhyroch <Anarhyroch@gmail.com>
2022-06-02 12:54:02 +0800

    Create.

---
+++ app/src/main/java/org/annatv/musicplayer/ui/dashboard/LocalViewModel.java
@@ -0,0 +1,19 @@
+package org.annatv.musicplayer.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class LocalViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public LocalViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/org/annatv/musicplayer/ui/notifications/LibraryFragment.java
@@ -0,0 +1,26 @@
+package org.annatv.musicplayer.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import org.annatv.musicplayer.R;
+
+public class LibraryFragment extends Fragment {
+
+    private LibraryViewModel libraryViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        libraryViewModel =
+                new ViewModelProvider(this).get(LibraryViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_library, container, false);
+        return root;
+    }
+}
